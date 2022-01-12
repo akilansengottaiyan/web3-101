33a577298bc7517320382a95068b3c0cd6dd1ae3
akilansengottaiyan <akilan0306@gmail.com>
2022-01-12 19:34:18 +0530

    read from config in migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,20 @@
 var Auction = artifacts.require('Auction.sol')
 var MockV3Aggregator = artifacts.require('mock/MockV3Aggregator.sol')
-const networkToPriceFeedAddress = {
-  rinkeby: '0x8a753747a1fa494ec906ce90e9f37563a8af630e',
-  kovan: '0x9326BFA02ADD2366b30bacB125260Af641031331',
-  'mainnet-fork': '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419'
-}
+
 // we do not need to deploy Participant and PlayerToken since Auction inherits both and we do not need their separate instance.
 module.exports = async function (deployer, network) {
+  let priceFeed
   if (network == 'development') {
     let mockAggregator = await MockV3Aggregator.deployed()
     if (!mockAggregator) {
       mockAggregator = await deployer.deploy(MockV3Aggregator, 8, 300000000000)
     }
-    networkToPriceFeedAddress['development'] = mockAggregator.address
+    priceFeed = mockAggregator.address
+  } else {
+    priceFeed = deployer[network].chainlinkConfig.priceFeed
   }
-  await deployer.deploy(Auction, networkToPriceFeedAddress[network])
+  if (!priceFeed) {
+    console.error('No chainlink Config not found for ', network)
+  }
+  await deployer.deploy(Auction, priceFeed)
 }
--- truffle-config.js
+++ truffle-config.js
@@ -7,13 +7,24 @@ module.exports = {
     development: {
       host: '127.0.0.1',
       port: 7545,
-      network_id: '*' // Match any network id
+      network_id: '*', // Match any network id
+      chainlinkConfig: {
+        apiRequest: {
+          oracle: '0x7AFe1118Ea78C1eae84ca8feE5C65Bc76CcF879e', //some dummy address since there is no oracle in the local
+          jobId:
+            '0x00000000000000000000000000000000b0bde308282843d49a3a8d2dd2464af1', //some dummy jobId since there is no oracle in the local
+          fees: 0.1 * 10 ** 8
+        }
+      }
     },
     'mainnet-fork': {
       host: '127.0.0.1',
       port: 8545,
       network_id: 1,
-      skipDryRun: true
+      skipDryRun: true,
+      chainlinkConfig: {
+        priceFeed: '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419'
+      }
     },
     rinkeby: {
       provider: () =>
@@ -25,7 +36,15 @@ module.exports = {
       gas: 5500000,
       confirmations: 0,
       timeoutBlocks: 200,
-      skipDryRun: true
+      skipDryRun: true,
+      chainlinkConfig: {
+        apiRequest: {
+          oracle: '0x7AFe1118Ea78C1eae84ca8feE5C65Bc76CcF879e',
+          jobId: 'b0bde308282843d49a3a8d2dd2464af1',
+          fees: 0.1 * 10 ** 18
+        },
+        priceFeed: '0x8a753747a1fa494ec906ce90e9f37563a8af630e'
+      }
     },
     kovan: {
       provider: () =>
@@ -37,7 +56,15 @@ module.exports = {
       gas: 5500000,
       confirmations: 0,
       timeoutBlocks: 200,
-      skipDryRun: true
+      skipDryRun: true,
+      chainlinkConfig: {
+        apiRequest: {
+          oracle: '0xc57b33452b4f7bb189bb5afae9cc4aba1f7a4fd8',
+          jobId: 'd5270d1c311941d0b08bead21fea7747',
+          fees: 0.1 * 10 ** 18
+        },
+        priceFeed: '0x9326BFA02ADD2366b30bacB125260Af641031331'
+      }
     }
   },
   compilers: {
